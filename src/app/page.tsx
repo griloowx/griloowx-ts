64a723b00daa697116bd93101ed684bbe9007739
Jhonnyssom <jhonnyssomux@gmail.com>
2024-07-24 14:37:25 -0300

    adjusts on `main` page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -273,30 +273,38 @@ export default async function Home() {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
-            Software designer, founder, and amateur astronaut.
+            Jhonnyssom Silva, 32, based in Curitiba, and a front-end developer.
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-            I’m Spencer, a software designer and entrepreneur based in New York
-            City. I’m the founder and CEO of Planetaria, where we develop
-            technologies that empower regular people to explore space on their
-            own terms.
+            I’m Jhonnyssom Silva, a front-end developer and amateur athlete
+            based in Curitiba. With a passion for crafting user experiences and
+            a love for sports, I balance my career at Nivello Tecnologia with my
+            hobbies, all while enjoying time with my family.
           </p>
           <div className="mt-6 flex gap-6">
-            <SocialLink href="#" aria-label="Follow on X" icon={XIcon} />
             <SocialLink
-              href="#"
+              href="https://x.com/griloowx"
+              aria-label="Follow on X"
+              icon={XIcon}
+              target="_blank"
+            />
+            <SocialLink
+              href="https://instagram.com/griloowx"
               aria-label="Follow on Instagram"
               icon={InstagramIcon}
+              target="_blank"
             />
             <SocialLink
-              href="#"
+              href="https://github.com/griloowx"
               aria-label="Follow on GitHub"
               icon={GitHubIcon}
+              target="_blank"
             />
             <SocialLink
-              href="#"
+              href="https://linkedin.com/in/jhonnyssomsilva"
               aria-label="Follow on LinkedIn"
               icon={LinkedInIcon}
+              target="_blank"
             />
           </div>
         </div>
