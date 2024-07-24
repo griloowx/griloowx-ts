db9ce82374524cf123971a93d316597108e3dc46
Jhonnyssom <jhonnyssomux@gmail.com>
2024-07-24 16:57:36 -0300

    deleting unused pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -189,7 +189,6 @@ function Resume() {
           <ArrowDownIcon className="h-4 w-4 stroke-zinc-400 transition group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50" />
         </Button>
       </div>
-     
     </>
   )
 }
